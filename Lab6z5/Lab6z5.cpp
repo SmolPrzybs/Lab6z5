7122596671d16cdb234f98572780ec6870e9f7bd
SmolPrzybs <maciek970@gmail.com>
2022-02-05 23:55:08 +0100

    Dodaj pliki projektów.

---
+++ Lab6z5/Lab6z5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+void potega(int* wsk_a, int* wsk_b, int* wsk_c) {
+    for (int i = 1; i < *wsk_b; i++) {
+        *wsk_a *= *wsk_c;
+    }
+}
+
+int main()
+{
+    int a, b, c;
+    cout << "Podaj liczbe" << endl;
+    cin >> a;
+    cout << "Podaj jej potege" << endl;
+    cin >> b;
+
+    c = a;
+
+    int* wsk_a = &a;
+    int* wsk_b = &b;
+    int* wsk_c = &c;
+
+    potega(wsk_a, wsk_b, wsk_c);
+
+    cout << "wynik potegowania to: " << *wsk_a;
+}
